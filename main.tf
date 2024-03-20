8fbbbf0ceef8036fb75fa323e2695c6d12c3d1d8
bhavesh <jainbhavesh2707@gmail.com>
2024-03-20 18:07:54 +0530

    adding dead letter

--- main.tf
+++ main.tf
@@ -9,4 +9,22 @@ resource "aws_sqs_queue" "forta_sqs_queue" {
   receive_wait_time_seconds = 20
   #deduplication_scope   = "messageGroup"
   #fifo_throughput_limit = "perMessageGroupId"
+  redrive_policy = var.enabled ? jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.forta_sqs_dlq_queue.arn
+    maxReceiveCount     = 4
+  }) : {}
+}
+
+resource "aws_sqs_queue" "forta_sqs_dlq_queue" {
+  name = var.name
+  count = var.enabled ? 1 : 0
+}
+
+resource "aws_sqs_queue_redrive_allow_policy" "terraform_queue_redrive_allow_policy" {
+  queue_url = aws_sqs_queue.forta_sqs_dlq_queue.id
+
+  redrive_allow_policy = jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.forta_sqs_queue.arn]
+  })
 }
--- variable.tf
+++ variable.tf
@@ -2,3 +2,8 @@ variable "name" {
   description = "The name of the SQS end with .fifo"
   default = "forta_sqs.fifo"
 }
+
+variable "enabled" {
+  type = bool
+  default = true
+}
