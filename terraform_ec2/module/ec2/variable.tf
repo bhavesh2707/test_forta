39335a17e4dbc472f3b71733a9203c7f510a4db8
bhavesh <jainbhavesh2707@gmail.com>
2024-04-19 15:50:49 +0530

    adding module for ec2

---
+++ terraform_ec2/module/ec2/variable.tf
@@ -0,0 +1,14 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "Name" {
+  type = string
+  default = "Forta-prod"
+}
